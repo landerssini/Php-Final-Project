6192ed435c66cd4363a79c08391a249233938044
Jose Torres <jota_torres@hotmaiil.es>
2023-02-03 09:48:04 +0100

    prueba PDO

--- db.php
+++ db.php
@@ -1,14 +1,40 @@
 <?php 
 
-  class Connection{
-      public $host = 'localhost';
-      public $username = 'root';
-      public $password = '';
-      public $db_name = 'favorites_movies';
+  // class Connection{
+  //     public $host = 'localhost';
+  //     public $username = 'root';
+  //     public $password = '';
+  //     public $db_name = 'favorites_movies';
 
-      public function connect(){
-        return mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
-      }
+  //     public function connect(){
+  //       return mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
+  //     }
+  // }
+
+
+  $host = "localhost";
+  $db = "favorites_movies";
+  $user = "root";
+  $password = "";
+  $charset = "utf8";
+
+  // DSN - Data Source Name
+
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+  // Opciones del PDO
+
+  $opciones = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ];
+
+  // Establecer conexión con la base de datos usando el DBH - DataBase Handle
+
+  try{
+      $dbh = new PDO($dsn, $user, $password, $opciones);
+  }catch(PDOException $e){
+      $e->getMessage();
   }
 
 
--- singup.php
+++ singup.php
@@ -2,33 +2,49 @@
 
     include_once("db.php");
 
-    class SingUp extends Connection  {
+//     class SingUp extends Connection  {
         
-        public function register($name, $email, $pass) {
-            $conexion = parent::connect();
-            $sql = "INSERT INTO users (id, name, email, password) VALUES (NULL,?,?,?)";
-            $query = $conexion->prepare($sql);
-            $query->bind_param('sss', $name, $email, $pass);
-            return $query->execute();
-        }
-    }
+//         public function register($name, $email, $pass) {
+//             $conexion = parent::connect();
+//             $sql = "INSERT INTO users (id, name, email, password) VALUES (NULL,?,?,?)";
+//             $query = $conexion->prepare($sql);
+//             $query->bind_param('sss', $name, $email, $pass);
+//             return $query->execute();
+//         }
+//     }
 
-    if(isset($_POST["send-register"])){
-        if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])){
-            echo "<p>Fields cannot be left empty.</p>";
-        }else{
-            $name_user = $_POST['name'];
-            $email = $_POST['email'];
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+//     if(isset($_POST["send-register"])){
+//         if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])){
+//             echo "<p>Fields cannot be left empty.</p>";
+//         }else{
+//             $name_user = $_POST['name'];
+//             $email = $_POST['email'];
+//             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
             
-            $user = new SingUp();
+//             $user = new SingUp();
             
-            if ($user->register($name_user, $email, $password)) {
-                header("location:index.php");
-            } else {
-                echo "Could not register";
-            }
-        }
+//             if ($user->register($name_user, $email, $password)) {
+//                 header("location:index.php");
+//             } else {
+//                 echo "Could not register";
+//             }
+//         }
+// }
+
+
+
+if(isset($_POST["send-register"])){
+    if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])){
+        echo "<p>No se pueden dejar campos vacíos.</p>";
+    }
+    else{
+        $pass_encrypt = password_hash($_POST["password"], PASSWORD_DEFAULT);
+      
+        $consulta = $dbh->prepare("INSERT INTO users (id, name, email, password) VALUES (NULL, ?, ?, ?)");
+        $consulta->execute([$_POST["name"], $_POST["email"], $pass_encrypt]);
+ 
+        $resultado = $consulta->rowCount();
+    }
 }
 
 ?>
